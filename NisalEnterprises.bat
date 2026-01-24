2edb23232eeaac0413de79ee1fbd63543f467343
Kevin Hirosh Dabarera <bsit241021@bci.lk>
2026-01-24 07:21:46 +0530

    error fixed

--- NisalEnterprises.bat
+++ NisalEnterprises.bat
@@ -1,3 +1,10 @@
 @echo off
+pushd "%~dp0"
+
+rem Start Flask server minimized
+start "NisalEnterprisesServer" /min cmd /c "python app.py"
+
+rem Give the server a moment, then open the app in the browser
+timeout /t 3 >nul
 start "" "http://127.0.0.1:5000/"
-exit
+popd
